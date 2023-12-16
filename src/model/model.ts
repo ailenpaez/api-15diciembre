d56a64cbac45ab7e1bb38a9acc8c706bc1c67bab
ailenpaez <a.jorgelinapaez@gmail.com>
2023-12-15 21:00:21 -0300

    Creando la base de la primer función del Model

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,9 +1,17 @@
-import {Provincias, LugaresTuristicos} from "../interfaces/interfaces"
+import { Provincia, LugarTuristico } from "../interfaces/interfaces";
 import { pathFile } from "../database/index";
 import jsonfile from "jsonfile";
 
+const getAllInfo = (): Provincia[] => jsonfile.readFileSync(pathFile);
 
-const getAllInfo = (): Provincias[] => jsonfile.readFileSync(pathFile);
-
-const leerDb = getAllInfo()
+const leerDb = getAllInfo();
 console.log(leerDb);
+
+// Crear un método getCitiesBySurface(surface) -> el mismo recibirá la supercie como parametro y deberá devolver
+// un array con todas las ciudades que posean mayor o igual superficie que la pasada por parametro. 
+//Los elementos del array deben tener solo tres propiedades: "nombre", "superficie_km2" y "habitantes".
+// [ { "nombre": "Buenos aires", "superficie_km2": 19283189, "habitantes": 12387192 }, { "nombre": "Santa Fe", "superficie_km2": 81289, "habitantes": 91919 } ]
+
+const getCitiesBySurface = (surface: any) => {
+
+}
